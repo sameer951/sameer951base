981962b7ecdc55328519d6f95fc5c772394eec86
Jyotikant <sanjay.row.com@gmail.com>
2020-06-15 17:48:10 +0530

    adding router to cv

--- projectCv/src/App.jsx
+++ projectCv/src/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { ResumePage } from './pagecomponents/Resume.page';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import Error404 from './pagecomponents/errorpage/error';
 
+// var addingRouter 
 function App() {
   return (
     <div className="App">
-     <ResumePage></ResumePage>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={withRouter((props) => {
+            props.history.push('/jyotikant_cv');
+            return <div>Hello</div>
+          })}></Route>
+          <Route exact path="/jyotikant_cv">
+            <ResumePage></ResumePage>
+          </Route>
+          <Route component={Error404} />
+        </Switch>
+      </Router>
     </div>
   );
+}
+function NoMatchPage() {
+  return <div>404 Error</div>
 }
 
 export default App;
