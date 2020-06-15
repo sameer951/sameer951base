016616c48904f64c32ba1c5f97e3cd9b2851a90d
Jyotikant <sanjay.row.com@gmail.com>
2020-06-15 18:24:57 +0530

    build updates

---
+++ precache-manifest.d104c4ffb644d6a4febcd3f43ea62f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd8a319f68765affe404326e1cf92c3",
+    "url": "/sameer951base/index.html"
+  },
+  {
+    "revision": "69a11521e4dcbe842cfa",
+    "url": "/sameer951base/static/css/2.2ff7fc03.chunk.css"
+  },
+  {
+    "revision": "69a11521e4dcbe842cfa",
+    "url": "/sameer951base/static/js/2.9b82eab6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sameer951base/static/js/2.9b82eab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf62a9ce2ab3d1e47ca",
+    "url": "/sameer951base/static/js/main.37faa029.chunk.js"
+  },
+  {
+    "revision": "5e119e10ba3b26571f3e",
+    "url": "/sameer951base/static/js/runtime-main.aae292c1.js"
+  }
+]);
