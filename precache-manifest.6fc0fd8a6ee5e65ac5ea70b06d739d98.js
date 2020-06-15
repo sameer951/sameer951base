3fdf44972084ac23736f3e2e8bffe4ea41dcba9f
Jyotikant <sanjay.row.com@gmail.com>
2020-06-15 18:51:59 +0530

    Updates

---
+++ precache-manifest.6fc0fd8a6ee5e65ac5ea70b06d739d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1839e7d7ebe562daaaa5c8f82b170f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a11521e4dcbe842cfa",
+    "url": "/static/css/2.2ff7fc03.chunk.css"
+  },
+  {
+    "revision": "69a11521e4dcbe842cfa",
+    "url": "/static/js/2.9b82eab6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9b82eab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be89b01b227ab9f3c381",
+    "url": "/static/js/main.6bb3b968.chunk.js"
+  },
+  {
+    "revision": "66d9e5fde78194f60a40",
+    "url": "/static/js/runtime-main.d397b310.js"
+  }
+]);
